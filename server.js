9654c73b4d10f589e232dc5e12ee166aa8e92c71
Jerum Hubbert <hubbertj@gmail.com>
2015-12-09 15:24:31 -0800

    01
    pushing code to master

---
+++ server.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var pPort = process.env['testPort'] || 8080;
+
+connect().use(serveStatic('www')).listen(8080, function() {
+    console.log('Starting server on port ' + pPort)
+});
+
