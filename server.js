981c0479c882352c756c8256ba7dcdcf8552c628
Jerum Hubbert <jerum.hubbert@scientificgames.com>
2017-01-10 15:42:49 -0800

    adding more to the stuff

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var pPort = process.env['testPort'] || 8080;
 
-connect().use(serveStatic('www')).listen(8080, function() {
-    console.log('Starting server on port ' + pPort)
+connect().use(serveStatic('www')).listen(8080, () => {
+	console.log('Starting server on port ' + pPort)
 });
 
