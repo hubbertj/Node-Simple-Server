77b2c6f5d9721dd8c7866d11832b0b9f9c259f12
Jerum Hubbert <jerum.hubbert@scientificgames.com>
2017-01-12 14:51:02 -0800

    adding new es6 changes

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 
 var connect = require('connect');
 var serveStatic = require('serve-static');
-var pPort = process.env['testPort'] || 8080;
+var pPort = process.env['testPort'] || 8000;
 
-connect().use(serveStatic('www')).listen(8080, () => {
+connect().use(serveStatic('www')).listen(8000, () => {
 	console.log('Starting server on port ' + pPort)
 });
 
